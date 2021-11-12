8426e412f226ed014773034a620b4e1cdfa8912e
peterman <sysop@pfeiffer-privat.de>
2021-11-12 08:39:43 +0100

    Update parameter.h

--- parameter.h
+++ parameter.h
@@ -1,6 +1,6 @@
 struct Settings {
-  char ssid[15] = "Weissig2011";
-  char pass[15] = "VereinsHaus";
+  char ssid[15] = "devwifi";
+  char pass[15] = "devwifi1";
   char host[15] = "asm-connect";
   char edit_user[10] = "admin";
   char edit_pass[10] = "admin";  
